09fcf805371589f9ff293d8669c87ba5e9bb0b95
Rik Mikac <rikmikac@gmail.com>
2013-07-11 15:26:09 -0500

    some initial resharper clean up

--- AnotherCastle/Level.cs
+++ AnotherCastle/Level.cs
@@ -1,35 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Engine;
 using Engine.Input;
 using System.Windows.Forms;
 using System.Drawing;
-using Tao.OpenGl;
 
 namespace AnotherCastle
 {
     class Level
     {
-        Input _input;
+        readonly Input _input;
         PersistentGameData _gameData;
-        PlayerCharacter _playerCharacter;
-        TextureManager _textureManager;
-        ScrollingBackground _background;
-        ScrollingBackground _backgroundLayer;
+        readonly PlayerCharacter _playerCharacter;
         //List<Enemy> _enemyList = new List<Enemy>();
-        EnemyManager _enemyManager;
-        MapObjectManager _mapObjectManager;
-        MissileManager _missileManager = new MissileManager(new RectangleF(-1300 / 2, -750 / 2, 1300, 750));
-        EffectsManager _effectsManager;
-        Room[][] _rooms;
-        Room _currentRoom;
-        private List<CellTypes> _levelOne;
+        readonly EnemyManager _enemyManager;
+        readonly MapObjectManager _mapObjectManager;
+        readonly MissileManager _missileManager = new MissileManager(new RectangleF(-1300 / 2, -750 / 2, 1300, 750));
+        readonly EffectsManager _effectsManager;
+        readonly Room _currentRoom;
 
         public Level(Input input, TextureManager textureManager, PersistentGameData gameData)
         {
-            _levelOne = new List<CellTypes> { 
+            var levelOne = new List<CellTypes> { 
                 CellTypes.rock_wall, CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,
                 CellTypes.rock_wall, CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.rock_wall,    
                 CellTypes.rock_wall, CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.rock_wall,    
@@ -40,15 +32,15 @@ namespace AnotherCastle
                 CellTypes.rock_wall, CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.dirt_floor,  CellTypes.rock_wall,    
                 CellTypes.rock_wall, CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,  CellTypes.rock_wall,        
             };
-            var _levelTwo = new List<CellTypes>();
-            _currentRoom = new Room(textureManager, _levelOne);
+
+            _currentRoom = new Room(textureManager, levelOne);
             _input = input;
             _gameData = gameData;
-            _textureManager = textureManager;
-            _effectsManager = new EffectsManager(_textureManager);
-            _playerCharacter = new PlayerCharacter(_textureManager, _missileManager);
-            _enemyManager = new EnemyManager(_textureManager, _effectsManager, _missileManager, new ClientBounds { NorthBound = 400, SouthBound = -400, EastBound = -640, WestBound = 640, });
-            _mapObjectManager = new MapObjectManager(_textureManager, _effectsManager, _currentRoom);
+            var textureManager1 = textureManager;
+            _effectsManager = new EffectsManager(textureManager1);
+            _playerCharacter = new PlayerCharacter(textureManager1, _missileManager);
+            _enemyManager = new EnemyManager(textureManager1, _effectsManager, _missileManager, new ClientBounds { NorthBound = 400, SouthBound = -400, EastBound = -640, WestBound = 640, });
+            _mapObjectManager = new MapObjectManager(textureManager1, _effectsManager, _currentRoom);
             //_enemyList.Add(new Enemy(_textureManager, _effectsManager));
 
             //_background = new ScrollingBackground(textureManager.Get("background"));
@@ -69,7 +61,7 @@ namespace AnotherCastle
         {
             _missileManager.UpdatePlayerCollision(_playerCharacter);
 
-            foreach (Enemy enemy in _enemyManager.EnemyList)
+            foreach (var enemy in _enemyManager.EnemyList)
             {
                 if (enemy.GetBoundingBox().IntersectsWith(_playerCharacter.GetBoundingBox()))
                 {
@@ -80,13 +72,11 @@ namespace AnotherCastle
                 _missileManager.UpdateEnemyCollisions(enemy);
             }
 
-            foreach (MapObject mapObject in _mapObjectManager.MapObjectList)
+            foreach (var mapObject in _mapObjectManager.MapObjectList)
             {
-                if (mapObject.GetBoundingBox().IntersectsWith(_playerCharacter.GetBoundingBox()))
-                {
-                    mapObject.OnCollision(_playerCharacter);
-                    _playerCharacter.OnCollision(mapObject);
-                }
+                if (!mapObject.GetBoundingBox().IntersectsWith(_playerCharacter.GetBoundingBox())) continue;
+                mapObject.OnCollision(_playerCharacter);
+                _playerCharacter.OnCollision(mapObject);
             }
         }
         public void Update(double elapsedTime, double gameTime)
@@ -114,102 +104,79 @@ namespace AnotherCastle
             }
 
             // Get controls and apply to player character
-            double _x = 0;
-            double _y = 0;
+            double x = 0;
+            double y = 0;
 
             if (_input.Controller != null)
             {
-                _x = _input.Controller.LeftControlStick.X;
-                _y = _input.Controller.LeftControlStick.Y * -1;
+                x = _input.Controller.LeftControlStick.X;
+                y = _input.Controller.LeftControlStick.Y * -1;
             }
-            Vector controlInput = new Vector(_x, _y, 0);
+            var controlInput = new Vector(x, y, 0);
 
-            if (Math.Abs(controlInput.Length()) < 0.0001)
+            if (!(Math.Abs(controlInput.Length()) < 0.0001)) return;
+            // If the input is very small, then the player may not be using
+            // a controller; he might be using the keyboard.
+            var box = _playerCharacter.GetBoundingBox();
+
+            foreach (var mapObject in _mapObjectManager.MapObjectList)
             {
-                // If the input is very small, then the player may not be using
-                // a controller; he might be using the keyboard.
-                var box = _playerCharacter.GetBoundingBox();
-                var left = box.Left;
-                var right = box.Right;
-                var top = box.Top;
-                var bottom = box.Bottom;
-                var leftBlock = false;
-                var rightBlock = false;
-                var topBlock = false;
-                var bottomBlock = false;
+                var objectBox = mapObject.GetBoundingBox();
 
-                foreach (var mapObject in _mapObjectManager.MapObjectList)
-                {
-                    var objectBox = mapObject.GetBoundingBox();
+                //if (box.IntersectsWith(objectBox))
+                //{
+                //    if (!leftBlock && (Math.Abs(objectBox.Right - left) < Math.Abs(objectBox.Left - left)) && !(objectBox.Top > bottom || objectBox.Bottom < top))
+                //        leftBlock = true;
 
-                    //if (box.IntersectsWith(objectBox))
-                    //{
-                    //    if (!leftBlock && (Math.Abs(objectBox.Right - left) < Math.Abs(objectBox.Left - left)) && !(objectBox.Top > bottom || objectBox.Bottom < top))
-                    //        leftBlock = true;
+                //    if (!rightBlock && (Math.Abs(objectBox.Left - right) < Math.Abs(objectBox.Right - right)) && !(objectBox.Top > bottom || objectBox.Bottom < top))
+                //        rightBlock = true;
 
-                    //    if (!rightBlock && (Math.Abs(objectBox.Left - right) < Math.Abs(objectBox.Right - right)) && !(objectBox.Top > bottom || objectBox.Bottom < top))
-                    //        rightBlock = true;
+                //    if (!topBlock && (Math.Abs(objectBox.Bottom - top) < Math.Abs(objectBox.Top - top)) && !(objectBox.Left > right || objectBox.Right < left))
+                //        bottomBlock = true;
 
-                    //    if (!topBlock && (Math.Abs(objectBox.Bottom - top) < Math.Abs(objectBox.Top - top)) && !(objectBox.Left > right || objectBox.Right < left))
-                    //        bottomBlock = true;
+                //    if (!bottomBlock && (Math.Abs(objectBox.Top - bottom) < Math.Abs(objectBox.Bottom - bottom)) && !(objectBox.Left > right || objectBox.Right < left))
+                //        topBlock = true;
+                //}
 
-                    //    if (!bottomBlock && (Math.Abs(objectBox.Top - bottom) < Math.Abs(objectBox.Bottom - bottom)) && !(objectBox.Left > right || objectBox.Right < left))
-                    //        topBlock = true;
-                    //}
-
-                    Vector depth = RectangleExtensions.GetIntersectionDepth(box, objectBox);
+                var depth = box.GetIntersectionDepth(objectBox);
 
 
-                    if (depth != Vector.Zero)
-                    {
-                        double absDepthX = Math.Abs(depth.X);
-                        double absDepthY = Math.Abs(depth.Y);
+                if (depth == Vector.Zero) continue;
+                var absDepthX = Math.Abs(depth.X);
+                var absDepthY = Math.Abs(depth.Y);
 
-                        if (absDepthX > absDepthY)
-                        {
-                            // Resolve the collision along the X axis.
-                            _playerCharacter.HandleCollision(new Vector(0, depth.Y, 0));
-                        }
-                        else
-                        {
-                            // Resolve the collision along the Y axis.
-                            _playerCharacter.HandleCollision(new Vector(depth.X, 0, 0));                            
-                        }
-                        // Perform further collisions with the new bounds.
-                        //bounds = BoundingRectangle;
-                    }
-                }
-
-                if (_input.Keyboard.IsKeyHeld(Keys.Left))
-                {
-                    _playerCharacter.MoveLeft();
-                    if (leftBlock) controlInput.X = 0;
-                    else controlInput.X = -1;
-                }
-
-                if (_input.Keyboard.IsKeyHeld(Keys.Right))
-                {
-                    _playerCharacter.MoveRight();
-                    if (rightBlock) controlInput.X = 0;
-                    else controlInput.X = 1;
-                }
-
-                if (_input.Keyboard.IsKeyHeld(Keys.Up))
-                {
-                    _playerCharacter.MoveUp();
-                    if (topBlock) controlInput.X = 0;
-                    else controlInput.Y = 1;
-                }
-
-                if (_input.Keyboard.IsKeyHeld(Keys.Down))
-                {
-                    _playerCharacter.MoveDown();
-                    if (bottomBlock) controlInput.X = 0;
-                    else controlInput.Y = -1;
-                }
-
-                _playerCharacter.Move(controlInput * elapsedTime);
+                _playerCharacter.HandleCollision(absDepthX > absDepthY
+                    ? new Vector(0, depth.Y, 0)
+                    : new Vector(depth.X, 0, 0));
+                // Perform further collisions with the new bounds.
+                //bounds = BoundingRectangle;
             }
+
+            if (_input.Keyboard.IsKeyHeld(Keys.Left))
+            {
+                _playerCharacter.MoveLeft();
+                controlInput.X = -1;
+            }
+
+            if (_input.Keyboard.IsKeyHeld(Keys.Right))
+            {
+                _playerCharacter.MoveRight();
+                controlInput.X = 1;
+            }
+
+            if (_input.Keyboard.IsKeyHeld(Keys.Up))
+            {
+                _playerCharacter.MoveUp();
+                controlInput.Y = 1;
+            }
+
+            if (_input.Keyboard.IsKeyHeld(Keys.Down))
+            {
+                _playerCharacter.MoveDown();
+                controlInput.Y = -1;
+            }
+
+            _playerCharacter.Move(controlInput * elapsedTime);
         }
 
         public void Render(Renderer renderer)
